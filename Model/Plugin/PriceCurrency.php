25158e8ce9f77b7176c6c6785d37ca429e674d3c
BjoernChristensen <bc@adtention.dk>
2022-02-18 15:42:24 +0100

    Update PriceCurrency.php

--- Model/Plugin/PriceCurrency.php
+++ Model/Plugin/PriceCurrency.php
@@ -46,8 +46,8 @@ class PriceCurrency extends PriceFormatPluginAbstract
                 return $price;
             }
 
-            // return 123;
-            return (float)number_format(round($price, 2, PHP_ROUND_HALF_UP), 2, ',', '.');
+            return 123;
+            // return (float)number_format(round($price, 2, PHP_ROUND_HALF_UP), 2, ',', '.');
 
         } else {
             return $proceed($price);
